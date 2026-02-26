e14e2ec50d8a3f33b11b208e5bbf212ff6d1e5d5
imprashantjha <prashantjha1312@gmail.com>
2026-02-26 12:08:07 +0530

    Milestone 2 frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SkillBridgeMilestone2 from "./SkillBridgeMilestone2";
+
+function App() {
+  return (
+    <div className="App">
+      <SkillBridgeMilestone2 />
+    </div>
+  );
+}
+
+export default App;
