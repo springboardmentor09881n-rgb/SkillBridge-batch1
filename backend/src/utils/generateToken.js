9a8a261e0436905742c9b161782ffa473d1e7e74
anusha-b2803 <anushaboomi2006@gmail.com>
2026-03-01 13:18:09 +0530

    frontend-final branch

--- backend/src/utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const generateToken = (userId, role) => {
-  return jwt.sign({ userId, role }, process.env.JWT_SECRET, { expiresIn: '1h' })
-}
-
-module.exports = generateToken
